13e6b7b43e196f9b6fafae60781b877ccfe46b99
BigSotaa <andrewbulthuis@outlook.com>
2025-02-24 17:47:28 -0600

    Refactor Dockerfile and theHarvester.py for improved dependency management and platform compatibility

--- src/theHarvester/theHarvester/theHarvester.py
+++ src/theHarvester/theHarvester/theHarvester.py
@@ -1,25 +1,21 @@
 import asyncio
-import sys
+import platform
+import multiprocessing
 
-from theHarvester import __main__
+multiprocessing.freeze_support()
 
-
-def main():
-    platform = sys.platform
-    if platform == "win32":
-        # Required or things will break if trying to take screenshots
-        import multiprocessing
-
-        multiprocessing.freeze_support()
-        asyncio.DefaultEventLoopPolicy = asyncio.WindowsSelectorEventLoopPolicy
-    else:
+if platform.system() == "Windows":
+    asyncio.DefaultEventLoopPolicy = asyncio.WindowsSelectorEventLoopPolicy()
+else:
+    try:
         import uvloop
-
         uvloop.install()
+    except ImportError:
+        print("uvloop is not installed. Please install it using 'pip install uvloop'.")
 
-        if "linux" in platform:
-            import aiomultiprocess
+    if "linux" in platform.system().lower():
+        import aiomultiprocess
 
-            # As we are not using Windows, we can change the spawn method to fork for greater performance
-            aiomultiprocess.set_context("fork")
+        # As we are not using Windows, we can change the spawn method to fork for greater performance
+        aiomultiprocess.set_context("fork")
     asyncio.run(__main__.entry_point())
